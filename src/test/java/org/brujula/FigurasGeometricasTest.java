57abb43054e7a8de1f5ab4ac3e25b086004478e2
Antoni March Marti <amarch@brujula.es>
2019-04-12 11:48:14 +0200

    Se añadirán casos test para comprobar que la clase cuadrado y la herencia funcionan

--- src/test/java/org/brujula/FigurasGeometricasTest.java
+++ src/test/java/org/brujula/FigurasGeometricasTest.java
@@ -13,14 +13,28 @@ public class FigurasGeometricasTest
 {
     @Test
     public void figuraTest(){
+        /*
         Figura figura = new Figura();
         Figura figuraGeometrica = new Figura("nombre");
         figura.setNombre("Cuadrado");
         assertEquals("Cuadrado", figura.getNombre());
         assertEquals("nombre", figuraGeometrica.getNombre());
-        /*Probar si el metodo pintar funcionará, lugo lo convertiremos en abstracto.
-         figura.pintar();
-         figuraGeometrica.pintar();
+        Probar si el metodo pintar funcionará, lugo lo convertiremos en abstracto.
+        figura.pintar();
+        figuraGeometrica.pintar();
          */
+    }
+
+    @Test
+    public void figurasCuadradoTest(){
+        Figura cuadrado = new Cuadrado();
+        Cuadrado cuadradoC = new Cuadrado("Cuadrado");
+        cuadrado.setNombre("cuadrado");
+        assertEquals("cuadrado", cuadrado.getNombre());
+        assertEquals("Cuadrado", cuadradoC.getNombre());
+        cuadradoC.setNombre("rectamgulo");
+        assertEquals("recatngulo", cuadradoC.getNombre());
+        cuadrado.pintar();
+        cuadradoC.pintar();
     }
 }
