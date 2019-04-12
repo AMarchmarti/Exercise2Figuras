85158170bd30a9d990256dcb21ca2af26c9f4f9d
Antoni March Marti <amarch@brujula.es>
2019-04-12 11:37:47 +0200

    Se añaden más casos test para comprobar que todo va como lo esperado

--- src/test/java/org/brujula/FigurasGeometricasTest.java
+++ src/test/java/org/brujula/FigurasGeometricasTest.java
@@ -3,6 +3,7 @@ package org.brujula;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.brujula.Figuras.Figura;
 import org.junit.Test;
 
 /**
@@ -15,8 +16,10 @@ public class FigurasGeometricasTest
         Figura figura = new Figura();
         Figura figuraGeometrica = new Figura("nombre");
         figura.setNombre("Cuadrado");
-        assertEquals("cuadrado", figura.getNombre());
+        assertEquals("Cuadrado", figura.getNombre());
         assertEquals("nombre", figuraGeometrica.getNombre());
         //Probar si el metodo pintar funcionará, lugo lo convertiremos en abstracto.
+        figura.pintar();
+        figuraGeometrica.pintar();
     }
 }
