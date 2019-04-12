d945a207edc20b977213cdf9e42013750e327322
Antoni March Marti <amarch@brujula.es>
2019-04-12 12:27:48 +0200

    Se añade otra clase multinivel de herencia, pasa los casos test

---
+++ src/main/java/org/brujula/Figuras/Cuadrados/Rectangulo.java
@@ -0,0 +1,13 @@
+package org.brujula.Figuras.Cuadrados;
+
+public class Rectangulo extends Cuadrado {
+
+    //Constructor
+    public Rectangulo(){
+        super();
+    }
+
+    public Rectangulo(String nombre){
+        super(nombre);
+    }
+}
